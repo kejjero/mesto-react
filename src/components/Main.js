82e5605eb47f3c09006ec4da555379463f817e73
Maxim <kejerino@yandex.ru>
2022-04-29 11:41:21 +0800

    loading data in profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,38 @@
 import React from "react";
 import api from "../utils/api";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 // import ImagePopup from "./ImagePopup";
 
 function Main ({openEditProfile, openAddPlace, openAvatar}) {
+    const [userAvatar, setUserAvatar] = useState()
+    const [userName, setUserName] = useState('Имя')
+    const [userDescription, setUserDescription] = useState('Деятельность')
+
+    function changeUserAvatar (avatar) {
+        setUserAvatar(avatar)
+    }
+
+    function changeUserName (name) {
+        setUserName(name)
+    }
+
+    function changeUserDescription (description) {
+        setUserDescription(description)
+    }
+
     useEffect(() => {
-    Promise.all([api.getProfile(), api.getCards()])
+        Promise.all([api.getProfile(), api.getCards()])
         .then(([res, data]) => {
-            let userAvatar = res.avatar
+            changeUserAvatar(res.avatar)
+            changeUserName(res.name)
+            changeUserDescription(res.about)
             // data.reverse();
-            console.log(userAvatar)
         })
         .catch((err) => {
             console.log(err)
         })
-    }, [])
-
+    },[])
 
     return (
         <main className="content">
@@ -29,13 +45,13 @@ function Main ({openEditProfile, openAddPlace, openAvatar}) {
                     />
                     <div
                         className="profile__avatar"
-                        style={{ backgroundImage: `url(${"userAvatar"})` }}
+                        style={{ backgroundImage: `url(${userAvatar})` }}
                     >
                     </div>
                 </div>
                 <div className="profile__info">
-                    <h1 className="profile__person">Имя</h1>
-                    <p className="profile__about-me">Деятельность</p>
+                    <h1 className="profile__person">{userName}</h1>
+                    <p className="profile__about-me">{userDescription}</p>
                     <button
                         type="button"
                         className="profile__edit-button"
