bb34be581ec2a1852dd06bb0490f03457906b546
Maxim <kejerino@yandex.ru>
2022-05-07 23:38:53 +0800

    build: user context created

---
+++ src/contexts/CurrentUserContext.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const userContext = {
+        name: 'Имя',
+        about: 'Деятельность',
+        avatar: 'https://goo.su/5KqwD',
+}
+
+export const CurrentUserContext = React.createContext(userContext);
