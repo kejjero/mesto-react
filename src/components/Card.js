eeb5dd5c3b7ba5124bf3369d9f70cb123d3d3b7d
Maxim <kejerino@yandex.ru>
2022-05-09 23:41:14 +0800

    fix: the entire card is passed to Main, not the selected fields

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-function Card({openDeleteCard, title, link, likes, handleCardClick}) {
+function Card({openDeleteCard, card, currentUser, handleCardClick, onCardLike}) {
+
+    // Определяем, являемся ли мы владельцем текущей карточки
+    const isOwn = card.owner._id === currentUser._id;
+
+    // Создаём переменную, которую после зададим в `className` для кнопки удаления
+    const cardDeleteButtonClassName = (
+        `element__trash ${isOwn ? 'element__trash_active' : 'element__trash_hidden'}`
+    );
+
+    // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+
+    // Создаём переменную, которую после зададим в `className` для кнопки лайка
+    const cardLikeButtonClassName =  `element__like ${isLiked ? 'element__like_active' : 'element__like_hidden'}`;
 
     function handleClick() {
-        handleCardClick({name: title, image: link})
+        handleCardClick({name: card.name, image: card.link})
+    }
+
+    function handleLikeClick() {
+        onCardLike(card)
     }
 
     return (
@@ -12,7 +30,7 @@ function Card({openDeleteCard, title, link, likes, handleCardClick}) {
         >
             <div className="element__contain">
                 <button
-                    className="element__trash element__trash_active"
+                    className={cardDeleteButtonClassName}
                     type="button"
                     onClick={openDeleteCard}
                 >
@@ -20,16 +38,16 @@ function Card({openDeleteCard, title, link, likes, handleCardClick}) {
                 <img
                     className="element__image"
                     onClick={handleClick}
-                    alt={title}
-                    src={link}
+                    alt={card.title}
+                    src={card.link}
                 />
             </div>
             <div className="element__description">
-                <h2 className="element__title">{title}</h2>
+                <h2 className="element__title">{card.name}</h2>
                 <div className="element__like-wrapper">
-                    <button type="button" className="element__like">
+                    <button type="button" className={cardLikeButtonClassName} onClick={handleLikeClick}>
                     </button>
-                    <span className="element__like-value">{likes.length}</span>
+                    <span className="element__like-value">{card.likes.length}</span>
                 </div>
             </div>
         </article>
